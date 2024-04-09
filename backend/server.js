5f6ee9802ba9762e42a42967cd15a273e8e6a0e0
Jay Dinesh Rane <jayu.rane32003@gmail.com>
2024-04-09 13:02:58 +0530

    npm package

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,7 @@ app.get('/api/home', async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT  = 5000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
