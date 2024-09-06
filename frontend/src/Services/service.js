f3cab021259fc09956cdb60654211895035a661d
Jay Dinesh Rane <jayu.rane32003@gmail.com>
2024-09-06 18:27:15 +0530

    Fix local and production

--- frontend/src/Services/service.js
+++ frontend/src/Services/service.js
@@ -1,8 +1,5 @@
-// export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://task-manager-id0g.onrender.com";
-// const BASE_URL = import.meta.env.MODE === 'production'
-//     ? import.meta.env.VITE_API_URL
-//     : 'http://localhost:5173'; 
-// const BASE_URL = "http://localhost:5173";
+const BASE_URL = import.meta.env.MODE === 'production'
+    ? 'https://task-manager-id0g.onrender.com'
+    : 'http://localhost:5000';
 
-export default BASE_URL;
+export { BASE_URL };
